864b90a5ec1cbcdc092b33fb8160a3b8b7551ba2
Zoobastiks <zoobastiks@example.com>
2026-06-03 00:46:01 +0300

    ZChess update 03.06.2026  0:46:01,38

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Auto-reload when new version detected
    ============================================= */
 
-const CACHE_VERSION = 'zchess-315952665818330';
+const CACHE_VERSION = 'zchess-122893814960';
 const STATIC_CACHE = `zchess-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `zchess-dynamic-${CACHE_VERSION}`;
 
