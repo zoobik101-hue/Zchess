b6f0d594f20c9b7bb3b1d44223588e54909c4726
Zoobastiks <zoobastiks@example.com>
2026-06-03 00:16:18 +0300

    ZChess update 03.06.2026  0:16:18,13

--- js/config.js
+++ js/config.js
@@ -26,7 +26,7 @@ window.ZChess.firebaseConfig = {
 // ==========================================
 // APP CONSTANTS
 // ==========================================
-window.ZChess.BUILD = '26370324089352';
+window.ZChess.BUILD = '26958104113639';
 window.ZChess.VERSION = '1.1.5';
 window.ZChess.APP_NAME = 'ZChess';
 window.ZChess.SITE_URL = 'https://zoobik101-hue.github.io/Zchess/';
--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Auto-reload when new version detected
    ============================================= */
 
-const CACHE_VERSION = 'zchess-26370324089352';
+const CACHE_VERSION = 'zchess-26958104113639';
 const STATIC_CACHE = `zchess-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `zchess-dynamic-${CACHE_VERSION}`;
 
